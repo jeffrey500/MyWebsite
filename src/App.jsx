e36cbca42a21ce4e5b746e26f220c21ede68c841
jeffrey Zhu <jeffreyzhu11@gmail.com>
2026-01-25 21:46:22 -0500

    comming soon page

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,21 @@ import './App.css'
 
 function App() {
   return (
-      <div>
-          <div className="border-5 border-blue-500 m-4 p-4 bg-blue-300 rounded-lg">
-              What
-          </div>
+      <div className="min-h-screen flex items-center justify-center">
+          <button className="
+        outline-blue-500
+        outline-2
+        outline-offset-4
+        outline-solid
+        py-20
+        px-50
+        rounded-lg
+        bg-blue-200
+        text-9xl
+        text-blue-900
+      ">
+              Coming Soon!
+          </button>
       </div>
   )
 }
