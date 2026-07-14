06a24e1f05e7bac5892dbe7566dd79a98f9754ea
jeffrey Zhu <jeffreyzhu11@gmail.com>
2026-07-14 11:47:06 -0400

    Edited landing page and added svgs

--- src/Landing.jsx
+++ src/Landing.jsx
@@ -1,4 +1,5 @@
 import videoSource from './assets/jzhu.mp4';
+import arrow from './assets/down_arrow.svg?url';
 
 function Landing(){
     return(
@@ -12,8 +13,25 @@ function Landing(){
                 >
                     <source src={videoSource} type="video/mp4"/>
                 </video>
-                <div className="relative z-10 flex h-full items-center justify-center">
-                    <p className="-translate-x-5 z-10 text-5xl text-[#7e8ea7] font-sans font-bold ml-15 mt-50">Work In Progress</p>
+
+                <div className="relative z-10 flex flex-col h-full items-center justify-center translate-x-10 translate-y-5">
+                    <div className="relative bg-gray-300 rounded-xl p-5 text-5xl shadow-xl">
+                        <p className="text-[#7e8ea7] font-sans font-bold">Engineering Science @ UofT</p>
+                    </div>
+
+                    <div className="flex flex-row items-center justify-center translate-y-10 text-4xl text-[#7e8ea7] font-bold">
+                        <a href={"#projects"}>
+                            <div className="relative bg-blue-200 rounded-xl p-3 -translate-x-15 shadow-xl">Projects</div>
+                        </a>
+
+                        <a href={"#contact"}>
+                            <div className="relative bg-blue-200 rounded-xl p-3 translate-x-15 shadow-xl">Contact</div>
+                        </a>
+                    </div>
+
+                    <a href={"#about"} className="animate-bounce translate-y-40">
+                        <img src={arrow} className="h-20 w-20" alt="Scroll down"/>
+                    </a>
                 </div>
             </div>
         </section>
--- src/ProjectCard.jsx
+++ src/ProjectCard.jsx
@@ -1,3 +1,5 @@
+import link from "./assets/link.svg";
+
 const ProjectCard = ({github_link, color, title, description, imageURL, tech1, tech2, tech3, date}) => {
     return (
         <a
@@ -5,16 +7,19 @@ const ProjectCard = ({github_link, color, title, description, imageURL, tech1, t
             target="_blank"
             rel="noopener noreferrer"
             className={`bg-white rounded-4xl shadow-sm hover:shadow-2xl duration-500 border-6 ${color} p-7 my-7 grid grid-cols-[2fr_1fr] gap-2`}>
-                <div className="flex flex-col">
+            <div className="flex flex-col">
+                <div className="flex flex-row">
                     <h2 className="text-3xl font-bold text-left pb-5">{title}</h2>
-                    <p className="text-gray-500 text-gray-600 text-left max-w-130">{description}</p>
-                    <div className="flex flex-row mt-7 font text-sm">
-                        <div className="mr-3 py-1 px-2 bg-gray-200 rounded-md">{tech1}</div>
-                        <div className="mr-3 py-1 px-2 bg-gray-200 rounded-md">{tech2}</div>
-                        <div className="mr-3 py-1 px-2 bg-gray-200 rounded-md">{tech3}</div>
-                        <div className="mr-3 py-1 px-2 bg-gray-200 rounded-md">{date}</div>
-                    </div>
+                    <img src={link} className="h-7 w-7 translate-x-3 translate-y-1" alt="link-out"/>
                 </div>
+                <p className="text-gray-500 text-gray-600 text-left max-w-130">{description}</p>
+                <div className="flex flex-row mt-7 font text-sm">
+                    <div className="mr-3 py-1 px-2 bg-gray-200 rounded-md">{tech1}</div>
+                    <div className="mr-3 py-1 px-2 bg-gray-200 rounded-md">{tech2}</div>
+                    <div className="mr-3 py-1 px-2 bg-gray-200 rounded-md">{tech3}</div>
+                    <div className="mr-3 py-1 px-2 bg-gray-200 rounded-md">{date}</div>
+                </div>
+            </div>
 
             <div
                 className="h-50 w-65 overflow-hidden justify-self-end flex items-center justify-center">
